2b145e43c496f812bd95b23839afcbdfd124bd4a
Adam McDaniel <adam.mcdaniel17@gmail.com>
2026-01-29 15:05:27 -0500

    Fixed path for detailed sim file

--- profiler-hook.sh
+++ profiler-hook.sh
@@ -3,4 +3,4 @@ echo "RUNNING PROFILER HOOK."
 
 __HOOK_SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-exec $__HOOK_SCRIPT_DIR/library-interceptors/perf-libs-tools/tools/perf-libs-run --sync-gpu --detailed-sim --detailed-sim-file $ROCHPL_ROOT/adam_sim_markers.txt --probe "$@"
+exec $__HOOK_SCRIPT_DIR/library-interceptors/perf-libs-tools/tools/perf-libs-run --sync-gpu --detailed-sim --detailed-sim-file ./adam_sim_markers.txt --probe "$@"
